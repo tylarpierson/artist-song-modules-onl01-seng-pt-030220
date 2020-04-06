dd120550450db00b8a59bc0f47235155a39ffede
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 18:33:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
 
   @@songs = [] 
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
