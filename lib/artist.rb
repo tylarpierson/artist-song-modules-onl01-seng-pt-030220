8ff3630a9b56e7fc916bb88c8be78dc6abda08a2
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 18:35:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,6 @@ class Artist
   extend Memorable::ClassMethods 
   include Memorable::InstanceMethods
   include Paramable 
-  include Findable
   attr_accessor :name
   attr_reader :songs
 
@@ -15,9 +14,9 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def self.all
     @@artists
@@ -31,8 +30,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
