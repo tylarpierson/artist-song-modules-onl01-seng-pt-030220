5e699490ecb210dbb5c5bfe06be0e9e46cb6d0b7
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 18:30:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods 
+  include Memorable::InstanceMethods
   include Paramable 
   attr_accessor :name
   attr_reader :songs
